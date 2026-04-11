e1942091722f3a9ca5b4f899d22e96cc0354e9d3
Lucas Anselmo <luc_anselmo@hotmail.com>
2026-04-11 14:24:51 +0100

    feat: arp_scan collection with parsing

--- net-watcher/collectors/arp_scan.py
+++ net-watcher/collectors/arp_scan.py
@@ -12,20 +12,55 @@ OUI_FILE = "/usr/share/arp-scan/ieee-oui.txt"
 MAC_VENDOR_FILE = "/etc/arp-scan/mac-vendor.txt"
 
 
-def get_current_devices() -> list[dict]:
+def get_raw_scan() -> str:
+    """
+    Fetches the raw arp-scan data
+
+    Args:
+        None
+
+    Returns:
+        Scan result in str format
+    """
+
+    cmd = ["arp-scan", f"--ouifile={OUI_FILE}", f"--macfile={MAC_VENDOR_FILE}", "--localnet"]
+    
+    scan = subprocess.run(cmd, capture_output=True, text=True)
+
+    return scan.stdout
+
+def get_current_devices(raw_output: str=None) -> list[dict]:
     """
     Get the current active devices
 
     Args:
-        none
+        Raw output of a arp-scan, default it's the function scanning itself
 
     Returns:
         A list of dicts, with the device IP, MAC and Vendor
     """
+    result = []
+    if raw_output is None:
+        raw_output=get_raw_scan()
 
-    pass
+    arp_scan = raw_output.splitlines()
+
+    for line in arp_scan:
+        parts = line.split("\t")
+
+        if len(parts) == 3:
+            ip, mac, vendor = parts
+            dict_result = {"ip":ip, "mac":mac, "vendor":vendor}
+            result.append(dict_result)
 
 
+    return result
+
+
+
+# Check if is in main
 if __name__ == "__main__":
+    raw_scan = get_raw_scan()
     curr_devices = get_current_devices()
     print("Number of current devices", curr_devices)
+    print("Raw scan: ", raw_scan)
